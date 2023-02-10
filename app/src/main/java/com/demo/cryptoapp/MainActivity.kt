21a0ccd3414a74ea19a3dbb4aadbc2d441a18896
Lisandro Carvajalino <lisandrocarvajalino@wposs.com>
2023-02-10 09:20:14 -0500

    Implementación - Diseño pantalla de bienvenida
    - Se crea el diseño de la pantalla de bienvenida
    - Se agrega nueva actividad para mostrar la lista de criptomonedas
    - Se agrega funcionalidad al boton "Acces wallet" para que muestre la siguiente actividad

--- app/src/main/java/com/demo/cryptoapp/MainActivity.kt
+++ app/src/main/java/com/demo/cryptoapp/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.demo.cryptoapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.demo.cryptoapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+        mainBinding.btnAccess.setOnClickListener {
+            startActivity(
+                Intent(
+                    this@MainActivity,
+                    HomeActivity::class.java
+                )
+            )
+        }
+
     }
 }
