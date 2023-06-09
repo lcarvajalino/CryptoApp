4977f77abbcc133b8ebcd8c5af909b687fce0aa1
Lisandro Carvajalino <lisandrocarvajalino@wposs.com>
2023-06-09 15:58:44 -0500

    segundo commit

--- app/src/main/java/com/demo/cryptoapp/interfaces/details/IPresenterDetails.kt
+++ app/src/main/java/com/demo/cryptoapp/interfaces/details/IPresenterDetails.kt
@@ -1,12 +1,6 @@
 package com.demo.cryptoapp.interfaces.details
 
 import com.demo.cryptoapp.models.DetailsCoin
-
-interface IPresenterDetails {
-
-    fun getCoinDetails(id:String)
-
-    fun showResults(detailsCoin: DetailsCoin)
-    fun showError()
+holas
 
 }
